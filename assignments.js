d510344c7efd0a7dfc1a313063724b17c5124096
EOGKevin <104348441+EOGKevin@users.noreply.github.com>
2023-04-27 11:41:21 +0200

    Added JS code

---
+++ assignments.js
@@ -0,0 +1,17 @@
+"use strict";
+
+/*
+function describeCountry(country, population, capitalCity) {
+  const text = `${country} has ${population} people and its capital city is ${capitalCity}`;
+  return text;
+}
+
+const denmark = describeCountry("Denmark", 5900000, "Copenhagen");
+console.log(denmark);
+
+const sweden = describeCountry("Sweden", 10400000, "Stockholm");
+console.log(sweden);
+
+const norway = describeCountry("Norway", 5400000, "Oslo");
+console.log(norway);
+*/
