77d09f691c4dc8fd309244901421e2eb091331d4
EOGKevin <104348441+EOGKevin@users.noreply.github.com>
2023-04-27 11:57:55 +0200

    added new js code

--- script.js
+++ script.js
@@ -35,3 +35,18 @@ console.log(appleOrangeJuice);
 
 const num = Number("23");
 */
+
+function calcAge1(birthYear) {
+  return 2037 - birthYear;
+}
+
+const age1 = calcAge1(1991);
+console.log(age1);
+
+const calcAge2 = function (birthYear) {
+  return 3037 - birthYear;
+};
+
+const age2 = calcAge2(1991);
+
+console.log(age1, age2);
